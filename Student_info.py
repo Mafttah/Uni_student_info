0691aba6439cb7a12db53268a89ec95276d5ddee
Bora Saglam <posarobit@gmail.com>
2025-06-22 12:47:43 +0300

    Student_info.py

---
+++ Student_info.py
@@ -0,0 +1,20 @@
+universite = [ 
+    { 
+        "student_name": "Alex",
+        "student_age": "22" ,
+        "student_course": "Civil Engineer",
+        "student_lecture_note": "56"
+    },
+    {
+        "student_name": "Pedro",
+        "student_age": "25" ,
+        "student_course": "Machine Engineer",
+        "student_lecture_note": "60"
+    },
+    {
+        "student_name": "Martha",
+        "student_age": "19" ,
+        "student_course": "Architecture",
+        "student_lecture_note": "70"
+    }
+]
